d6d5c80ee690ac7251b1d3e3fa69873e1dce1ae5
stevenmquinn16 <33935225+stevenmquinn16@users.noreply.github.com>
2017-12-15 17:40:31 -0700

    Add files via upload
    Sam, here are the files

--- avalanche_map.py
+++ avalanche_map.py
@@ -3,7 +3,7 @@ import matplotlib.pyplot as plt
 from mpl_toolkits import mplot3d
 from stl import mesh
 import random
-
+import copy
 
 
 '''
@@ -18,11 +18,14 @@ Assumptions:
 class AvalancheMap:
 
     def __init__(self, map_path=None, voxel_delta=1):
-        self.rows    = None
-        self.cols    = None
-        self.heights = None
-
-        self.a_map   = None
+        self.rows          = None
+        self.cols          = None
+        self.heights       = None
+        self.beacon_map    = []
+        self.seen_beacons  = []
+        self.found_beacons = []
+        self.a_map         = None
+        self.search_actions = ['f','b','l','r']
 
         # stl file members
         self.x_range = None               # stl min to max w/ step size delta: x axis
@@ -373,7 +376,6 @@ class AvalancheMap:
             the second value is the distance to the beacon.
         '''
         #create the beacon map
-        beacon_map = []
         for x in xrange(self.cols):
             row_vector = []
             for y in xrange(self.rows):
@@ -383,7 +385,6 @@ class AvalancheMap:
                     beacon_visibility_array = []
                     for beacon in beacon_array:
                         distance_to_beacon = ((x-beacon[0])**2+(y-beacon[1])**2+(z-beacon[2])**2)**0.5
-                        meh = 1
                         if distance_to_beacon <= beacon[3]:
                             visable = True
                         else:
@@ -391,10 +392,10 @@ class AvalancheMap:
                         beacon_visibility_array.append((visable,distance_to_beacon))
                     height_vector.append(beacon_visibility_array)
                 row_vector.append(height_vector)
-            beacon_map.append(row_vector)
-        return beacon_map
+            self.beacon_map.append(row_vector)
+        return None
 
-    def plot_beacon_grid(self, beacon_location_vector, beacon_map):
+    def plot_beacon_grid(self, beacon_location_vector):
         '''plots the beacon_map and the grid_map'''
 
         x_max = self.cols
@@ -424,8 +425,10 @@ class AvalancheMap:
             for y in xrange(self.rows):
                 for z in xrange(self.heights):
                     i = 0
-                    for sight_flag in beacon_map [x][y][z]:
+                    for sight_flag in self.beacon_map [x][y][z]:
                         if sight_flag[0]:
+                            #print sight_flag
+                            #print self.a_map[x][y][z]
                             if i == 0:
                                 c = 'r'
                             elif i == 1:
@@ -440,11 +443,239 @@ class AvalancheMap:
                         i += 1
                     if self.a_map[x][y][z]:
                         ax2.scatter(x, y, z, c='k', marker = 's')
-
         ax2.set_ylim(0,y_max)
         ax2.set_xlim(0,x_max)
         ax2.set_zlim(0,z_max)
         ax2.set_xlabel('X_Distance')
         ax2.set_ylabel('Y_Distance')
         ax2.set_zlabel('Z_Height')
+        plt.show()
+
+    def opposite_action (self,a):
+        if a == 'u':
+            a_opposite = 'd'
+        elif a == 'd':
+            a_opposite = 'u'
+        elif a == 'r':
+            a_opposite = 'l'
+        elif a == 'l':
+            a_opposite = 'r'
+        elif a == 'f':
+            a_opposite = 'b'
+        elif a == 'b':
+            a_opposite = 'f'
+        else:
+            a_opposite = None
+        return a_opposite
+
+    def perpendicular_action (self,a):
+        if a == 'f':
+            a_perpendicular = 'r'
+        elif a == 'r':
+            a_perpendicular = 'b'
+        elif a == 'b':
+            a_perpendicular = 'l'
+        elif a == 'l':
+            a_perpendicular = 'u'
+        else:
+            a_perpendicular = None
+        return a_perpendicular
+
+    def transfer_func(self,s,a):
+        s_new = copy.deepcopy(s)
+        if a == 'u':    #up
+            s_new[2] += 1
+        elif a == 'd':  #down
+            s_new[2] -= 1
+        elif a == 'f':  #forward
+            s_new[1] += 1
+        elif a == 'b':  #backwards
+            s_new[1] -= 1
+        elif a == 'r':  #right
+            s_new[0] += 1
+        elif a == 'l':  #left
+            s_new[0] -= 1
+        elif a == 'ul': #up-left
+            s_new[0] -= 1
+            s_new[2] += 1
+        elif a == 'ur': #up-right
+            s_new[0] += 1
+            s_new[2] += 1
+        elif a == 'uf': #up-forward
+            s_new[1] += 1
+            s_new[2] += 1
+        elif a == 'ub': #ub-back
+            s_new[1] -= 1
+            s_new[2] += 1
+        elif a == 'df': #down-forward
+            s_new[1] += 1
+            s_new[2] -= 1
+        elif a == 'db': #down-back
+            s_new[2] -= 1
+            s_new[1] -= 1
+        elif a == 'dl': #down-left
+            s_new[0] -= 1
+            s_new[2] -= 1
+        elif a == 'dr': #down-right
+            s_new[0] += 1
+            s_new[2] -= 1
+        elif a == 'fl': #forward-left
+            s_new[0] -= 1
+            s_new[1] += 1
+        elif a == 'fr': #forward-right
+            s_new[0] += 1
+            s_new[1] += 1
+        elif a == 'bl': #back-left
+            s_new[0] -= 1
+            s_new[1] -= 1
+        elif a == 'br': #back-right
+            s_new[0] += 1
+            s_new[1] -= 1
+        else:
+            print 'invalid action'
+
+        #reset values that are outside of the map limits
+        if s_new[0] > self.cols:
+            s_new[0] = self.cols-1
+        if s_new[1] > self.rows:
+            s_new[1] = self.rows-1
+        if s_new[2] > self.heights:
+            s_new[2] = self.heights-1
+        if s_new[0] < 0:
+            s_new[0] = 0
+        if s_new[1] < 0:
+            s_new[1] = 0
+        if s_new[2] < 0:
+            s_new[2] = 0
+        return s_new
+
+    def line_search(self,s,cur_action,dist2beacon,active_beacon_index):
+        visited_states = []
+        s_prime = self.transfer_func(s, cur_action) #move to new step
+        visited_states.append(s_prime)
+        new_dist2beacon = self.beacon_map[s_prime[0]][s_prime[1]][s_prime[2]][0][1] #new distance to beacon
+        beacon_visibility = self.beacon_map[s_prime[0]][s_prime[1]][s_prime[2]][active_beacon_index][0] #checking beacon signal
+        while new_dist2beacon < dist2beacon and beacon_visibility:
+            dist2beacon = new_dist2beacon   #reassign minimum distance for comparison
+            s = s_prime #new current state
+            #take next step
+            s_prime = self.transfer_func(s, cur_action)
+            collision = self.a_map[s_prime[0]][s_prime[1]][s_prime[2]]  #check to see if s_prime in collision
+            # if s_prime is in collision, step up and keep trying to move laterally
+            if collision:   #s_prime is in collision
+                while collision and beacon_visibility:
+                    #step up
+                    action = 'u'
+                    s = self.transfer_func(s,action)
+                    temp_distance = self.beacon_map[s[0]][s[1]][s[2]][active_beacon_index][1]
+                    beacon_visibility = self.beacon_map[s[0]][s[1]][s[2]][active_beacon_index][0]
+                    if not beacon_visibility:
+                        break
+                    visited_states.append(s)
+                    #try to step forward
+                    s_prime = self.transfer_func(s,cur_action)
+                    collision = self.a_map[s_prime[0]][s_prime[1]][s_prime[2]]
+                    if collision:
+                        continue
+                    else:
+                        new_dist2beacon = self.beacon_map[s_prime[0]][s_prime[1]][s_prime[2]][active_beacon_index][1]
+                        if temp_distance < new_dist2beacon: #if spot before I went up was the minimum, then go back to that spot and exit.
+                            s_prime = s
+                            return s_prime,visited_states,temp_distance
+                        else:
+                            visited_states.append(s_prime) #add the s_prime to the visited array and continue on
+                            beacon_visibility = self.beacon_map[s_prime[0]][s_prime[1]][s_prime[2]][active_beacon_index][0]
+            else:
+                beacon_visibility = self.beacon_map[s_prime[0]][s_prime[1]][s_prime[2]][active_beacon_index][0]
+                if beacon_visibility:
+                    new_dist2beacon = self.beacon_map[s_prime[0]][s_prime[1]][s_prime[2]][active_beacon_index][1]
+                    visited_states.append(s_prime)
+        #if the beacon is lost due to an action, return to the state where you could see the beacon and declare
+        #this as the min point and exit
+        if not beacon_visibility:
+            s_prime = visited_states[-1]
+        return s_prime,visited_states, new_dist2beacon
+
+    def beacon_search(self,s,cur_action,dist2beacon,active_beacon_index):
+        '''  Inputs:
+            s = state that the drone is at when it first found the beacon
+            cur_action = action the drone just took to arrive at the state where it could see the beacon
+            active_beacon_index = Index of the beacon. the drone will keep searching this index in the beacon grid to make sure
+            the drone maintains connectivity to the antenna
+            dist2beacon = distance to the beacon from the cur_state
+        Returns:
+        '''
+        visited_states = []
+        if active_beacon_index in self.found_beacons:
+            return s,visited_states
+        if active_beacon_index not in self.seen_beacons:
+            self.seen_beacons.append(active_beacon_index)
+
+        #minimize in the first direction
+        s,minimize_visited_states,dist2beacon = self.line_search(s,cur_action,dist2beacon,active_beacon_index)
+        #print 'minimize_visited_states: ', minimize_visited_states
+        #print 'cur_action: ',cur_action
+        #print 'dist2beacon(after 1st minimization): ',dist2beacon
+
+        #determine perpendicular direction to move
+        a_perpendicular = self.perpendicular_action(cur_action)
+        #print 'a_perpendicular: ',a_perpendicular
+        s_prime = self.transfer_func(s, a_perpendicular)
+        minimize_visited_states.append(s_prime)
+        temp_distance = self.beacon_map[s_prime[0]][s_prime[1]][s_prime[2]][active_beacon_index][1]
+        #print 'temp_distance: ',temp_distance
+        #check if that was the right distance
+        if temp_distance >= dist2beacon:
+            #print 'perpendicular distance is wrong'
+            a_opposite = self.opposite_action(a_perpendicular)
+            #print 'a_opposite: ', a_opposite
+            minimize_visited_states.append(s)
+            s_prime = self.transfer_func(s, a_opposite)
+            dist2beacon = self.beacon_map[s_prime[0]][s_prime[1]][s_prime[2]][active_beacon_index][1]
+            #print 'dist2beacon: ', dist2beacon
+            minimize_visited_states.append(s_prime)
+            s = s_prime
+            cur_action = a_opposite
+        else:
+            #print 'perpendicular distance is correct'
+            cur_action = a_perpendicular
+            dist2beacon = temp_distance
+        #print 'cur_action: ', cur_action
+        #print 'dist2beacon(going into next line search): ',dist2beacon
+
+        #minimize in the second direction
+        s,minimize_visited_states2,dist2beacon = self.line_search(s,cur_action,dist2beacon,active_beacon_index)
+        #print 'minimize_visited_states2: ', minimize_visited_states2
+        visited_states = minimize_visited_states+minimize_visited_states2
+        self.found_beacons.append(active_beacon_index)
+        #print 'total path: ', visited_states
+        return s,visited_states
+
+    def plot_master_path(self,beacon_locations,visited_list):
+
+        x_max = self.cols
+        y_max = self.rows
+        z_max = self.heights
+        fig1 = plt.figure()
+        ax1 = fig1.add_subplot(111, projection='3d')
+        #plotting the beacons
+        for beacon in beacon_locations:
+            x = beacon[0]
+            y = beacon[1]
+            z = beacon[2]
+            ax1.scatter(x,y,z,c='b',marker='*')
+        #plotting the path
+        #print visited_list [0]
+        for states in visited_list:
+            x = states[0]
+            y = states[1]
+            z = states[2]
+            #print x,y,z
+            ax1.scatter(x,y,z,c='r',marker='o')
+        ax1.set_ylim(0,y_max)
+        ax1.set_xlim(0,x_max)
+        ax1.set_zlim(0,z_max)
+        ax1.set_xlabel('X_Label')
+        ax1.set_ylabel('Y_Label')
+        ax1.set_zlabel('Z_Label')
         plt.show()
--- avalanche_search_main.py
+++ avalanche_search_main.py
@@ -1,21 +1,72 @@
-import beacon_setup
-import math
 import avalanche_map as a_map
-
+import copy
 
 if __name__ == '__main__':
 
     #setting up grid maps and beacon maps
     simple_map = a_map.AvalancheMap(voxel_delta=0.5)
     simple_map.load_map('Maps\\default_map.txt')
-    num_beacons = 3
-    default_beacon_range = 15
-    avalanche_size = 'large' #enter 'small', 'medium' or 'large'
+    master_visited = []
+    num_beacons = 2
+    default_beacon_range = 180
+    avalanche_size = 'small' #enter 'small', 'medium' or 'large'
     #simple_map.beacon_location returns a list with length = num_beacons giving the x,y and z locations
     beacon_locations = simple_map.beacon_location(num_beacons,avalanche_size,default_beacon_range)
     print 'beacon locations: ', beacon_locations
 
     #simple_map.distance_calculator calculates the distances to each drone, if it is within the search range, it set to
     #true, otherwise, false.
-    beacon_map = simple_map.distance_calculator(beacon_locations)
-    simple_map.plot_beacon_grid(beacon_locations,beacon_map)
+    simple_map.distance_calculator(beacon_locations)
+    #simple_map.plot_beacon_grid(beacon_locations)
+    # Starting Point, active beacon, distance and action for testing
+    x = 0
+    y = 0
+    z = 12
+    current_action = 'r'
+    current_state = []
+    active_beacon_index = 0
+    current_state.append(x)
+    current_state.append(y)
+    current_state.append(z)
+    dist_2_beacon = simple_map.beacon_map[x][y][z][active_beacon_index][1]
+    #print 'dist_2_beacon: ', dist_2_beacon
+    #print 'initial cur_action: ', cur_action
+    #Local search for the beacon
+    master_visited = []
+    for beacons in beacon_locations:
+        print 'active beacon: ', beacon_locations[active_beacon_index]
+        #print 'active beacon index: ',active_beacon_index
+        beacon_visibility = simple_map.beacon_map[x][y][z][active_beacon_index][0]
+        if beacon_visibility:
+                current_state,minimized_path = simple_map.beacon_search(current_state,current_action, dist_2_beacon,active_beacon_index)
+    #            print minimized_path
+                master_visited = master_visited + minimized_path
+    #            active_beacon_index += 1
+    #             if active_beacon_index < len(beacon_locations):
+    #                 #print 'active beacon: ',beacon_locations[active_beacon_index]
+    #                 #print 'active beacon index: ', active_beacon_index
+    #                 dist_2_beacon = simple_map.beacon_map[current_state[0]][current_state[1]][current_state[2]][active_beacon_index][1]
+    #                 #find the right action to take for the next beacon
+    #                 options = []
+    #                 #find the best action to move in from current_state
+    #                 for action in simple_map.search_actions:
+    #                     possible_state = simple_map.transfer_func(current_state,action)
+    #                     possible_dist2next = simple_map.beacon_map[possible_state[0]][possible_state[1]][possible_state[2]][active_beacon_index][1]
+    #                     options.append([possible_state, action, possible_dist2next])
+    #                 #print 'current state:', current_state
+    #                 #print options
+    #                 d_min = 10000
+    #                 for d in options:
+    #                     d_choice = d[2]
+    #                     if d_choice <= d_min:
+    #                         current_state = d[0]
+    #                         current_action = d[1]
+    #                         dist_2_beacon = d[2]
+    #
+    #                 #print 'best: ',current_action,current_state,dist_2_beacon
+    #                 master_visited = master_visited + [current_state]
+    #                 meh = 2
+    print master_visited
+    simple_map.plot_master_path(beacon_locations,master_visited)
+
+    meh = 1
